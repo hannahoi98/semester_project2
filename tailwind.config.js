14bf38aeac63ba9f1476289914865a7cfffd6cb7
hannahoeidahl <hanhoi02118@stud.noroff.no>
2025-02-09 17:20:00 +0100

    Configuring Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#6A1B1B",
+        backgroundColor: "#FAF9F6",
+        textColor: "#3A3936",
+        successColor: "#0E6810",
+      },
+      fontFamily: {
+        heading: ["Bodoni Moda", "serif"],
+        body: ["Open Sans Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
